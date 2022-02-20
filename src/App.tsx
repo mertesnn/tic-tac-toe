ab7012ef84ad76ad292e5e2d605e37e67d001fc1
mertesnn <mert_esen_@hotmail.com>
2022-02-20 13:11:35 +0300

    Design draft added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
 import '@fontsource/montserrat';
+import { FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 import Theme from './Utils/Theme';
-import { ChakraProvider, Container, Text } from '@chakra-ui/react';
+import {
+  Box,
+  ChakraProvider,
+  Container,
+  Link,
+  Table,
+  Td,
+  Text,
+  Tr,
+} from '@chakra-ui/react';
 
 const App = () => {
   return (
     <ChakraProvider theme={Theme}>
-      <Container centerContent>
+      <Container
+        h="100vh"
+        bgGradient="linear(to-l, #FC7000, #FC9900)"
+        maxW="full"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="space-between"
+      >
         <Text fontSize="5xl">Tic Tac Toe</Text>
+        <Table maxW="xl">
+          <Tr>
+            <Td>1</Td>
+            <Td>2</Td>
+            <Td>3</Td>
+          </Tr>
+          <Tr>
+            <Td>4</Td>
+            <Td>5</Td>
+            <Td>6</Td>
+          </Tr>
+          <Tr>
+            <Td>7</Td>
+            <Td>8</Td>
+            <Td>9</Td>
+          </Tr>
+        </Table>
+        <Box
+          p="3"
+          w="100%"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          gap="6"
+        >
+          <Link href="https://www.instagram.com/mertesen__/" target="_blank">
+            <FaInstagram />
+          </Link>
+          <Link href="https://github.com/mertesnn" target="_blank">
+            <FaGithub />
+          </Link>
+          <Link href="https://www.linkedin.com/in/mert-esen/" target="_blank">
+            <FaLinkedinIn />
+          </Link>
+        </Box>
       </Container>
     </ChakraProvider>
   );
--- src/Utils/Theme.ts
+++ src/Utils/Theme.ts
@@ -3,8 +3,33 @@ import { extendTheme } from '@chakra-ui/react';
 const Theme = extendTheme({
   initialColorMode: 'light',
   useSystemColorMode: false,
+
+  styles: {
+    global: {
+      body: {
+        color: 'gray.800',
+      },
+      a: {
+        fontSize: '2rem',
+        _hover: {
+          color: 'gray.300',
+        },
+      },
+      td: {
+        padding: '50px !important',
+        borderLeft: '1px',
+        textAlign: 'center !important',
+      },
+      'td:nth-child(1)': {
+        borderLeft: '0px',
+      },
+      'tr:nth-child(3) td': {
+        borderBottom: '0px',
+      },
+    },
+  },
   fonts: {
-    Text: 'Montserrat',
+    body: 'Montserrat',
   },
 });
 
